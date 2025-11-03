8ac74f99ea959c213f65260e0c744ba78cec1e79
mrkless <mrklessbucag@gmail.com>
2025-11-03 23:11:49 +0800

    Fix project details display and images

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -24,12 +24,12 @@ const ProjectDetails = () => {
   }
 
   const images = project.Images || [
-    "proj/welcome.jpg",
-    "proj/continue.jpg",
-    "proj/signup.jpg",
-    "proj/verification.jpg",
-    "proj/dashboard.jpg",
-    "proj/profile.jpg",
+    "/proj/welcome.jpg",
+    "/proj/continue.jpg",
+    "/proj/signup.jpg",
+    "/proj/verification.jpg",
+    "/proj/dashboard.jpg",
+    "/proj/profile.jpg",
   ];
 
   return (
